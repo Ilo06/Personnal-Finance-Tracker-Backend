d5f3a0a42430e07604f008573d1bbcf0528aed5e
RRTRyan <tiaryanrzk@gmail.com>
2025-08-31 15:28:37 +0300

    fix: incorrect controller function for summary alert

--- src/routes/summary.route.js
+++ src/routes/summary.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-import requireAuth from "../middleware/auth.js"; 
+import requireAuth from "../middleware/auth.js";
 import * as summaryController from "../controllers/summary.controller.js";
 
 const summary = express.Router();
 summary.get("/", requireAuth, summaryController.getSummaryBetweenDates);
-summary.get("/monthly",requireAuth, summaryController.getMonthlySummary); 
-summary.get("/alerts",requireAuth, summaryController.getMonthlySummary);
+summary.get("/monthly", requireAuth, summaryController.getMonthlySummary);
+summary.get("/alerts", requireAuth, summaryController.getMonthlyAlerts);
 export default summary;
